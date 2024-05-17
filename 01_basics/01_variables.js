63c419dcbf5c48dfc9497833bc5c7b19d0159b22
Karan6299 <129037173+Karan6299@users.noreply.github.com>
2024-05-17 05:25:06 +0000

    save

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+// console.log("karan kumar");
+
+const accountId = "3257993493";
+
+let accountEmail = "karan@google.com";
+
+var accountPassword = "545454";
+
+accountCity = "Bihar";
+
+console.log(accountCity);
+
+// prefer not to use var 
+// because of issue in block scope and functional scope
+
+// console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// alert("hello");
+
+console.log(3 + 3);
+
+console.log("karan");
+
+let name = "karan"
+let age = 23
+let isLoggedIn = false;
+
+
+//number => to power 53
+//bigint 
+//string = "karan"
+//boolean = true/false
+//null = standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
